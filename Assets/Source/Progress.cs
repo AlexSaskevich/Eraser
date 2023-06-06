a53adb33cbbdf1fafb29b3502ea6ab3eb19461fd
AlexSaskevich <saskevich.aleksey@gmail.com>
2023-06-06 20:09:51 +0300

    Upload working version

---
+++ Assets/Source/Progress.cs
@@ -0,0 +1,37 @@
+﻿using ScratchCardAsset;
+using System;
+using UnityEngine;
+
+namespace Assets.Source
+{
+    public class Progress : MonoBehaviour
+    {
+        [SerializeField] private ScratchCardManager _cardManager;
+        [Range(0, 1)][SerializeField] private float _progressPercentToComplete;
+
+        private void OnEnable()
+        {
+            _cardManager.Progress.OnProgress += OnScratchProgress;
+        }
+
+        private void OnDisable()
+        {
+            _cardManager.Progress.OnProgress -= OnScratchProgress;
+        }
+
+        private void Update()
+        {
+            if (_cardManager.Card.IsScratching == false)
+                _cardManager.Card.ClearInstantly();
+        }
+
+        private void OnScratchProgress(float progress)
+        {
+            if (progress >= _progressPercentToComplete)
+            {
+                _cardManager.Card.FillInstantly();
+                _cardManager.Card.InputEnabled = false;
+            }
+        }
+    }
+}
---
+++ Assets/Source/Restart.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Source
+{
+    public class Restart : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnButtonClick);
+        }
+
+        private void OnButtonClick()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
